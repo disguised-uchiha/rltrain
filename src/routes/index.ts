0455fd5b7811b76bcab026cf69be2ce7ef2eeb24
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-11-28 17:43:15 +0530

    added train add route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
+import { trainModel } from "src/models/train";
 
 const router = Router();
 
-router.post("/add-train", (req: Request, res: Response) => {
+router.post("/add-train", async (req: Request, res: Response) => {
   const { train_no, train_name, source, destination, fare, availability } = req.body;
-  console.log("🚀 >>> train_no, train_name, source, destination, fare, availability", train_no, train_name, source, destination, fare, availability);
-  res.status(200).json({ message: "success" });
+  const resp = await trainModel.create({ train_no, train_name, source, destination, fare, availability });
+  return res.status(200).send(resp);
 });
 
 export { router };
