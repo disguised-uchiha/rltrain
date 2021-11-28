b9b7f0fe7c6a8a7fc3731521ba72cacdf66af3cd
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-11-28 17:13:41 +0530

    dotenv config

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
-import { logger } from "@utils/logger";
 
 dotenv.config({
   path: `.env.${process.env.NODE_ENV || "development"}`
 }); // development default
 
-logger.info(`USING ENVIRONMENT: ${process.env.NODE_ENV}`);
+console.log(`USING ENVIRONMENT: ${process.env.NODE_ENV}`);
