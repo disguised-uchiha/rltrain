26b0650277792fab83d5ad666121eaff65c24e3a
BhangalePratik <pratikbhangale760@gmail.com>
2021-11-28 18:25:26 +0530

    delete operation added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,47 @@
-import { Router, Request, Response } from "express";
+import {
+  Router,
+  Request,
+  Response
+} from "express";
 
 const router = Router();
+const Train = require('../models/train');
+
 
 router.post("/add-train", (req: Request, res: Response) => {
-  const { train_no, train_name, source, destination, fare, availability } = req.body;
+  const {
+    train_no,
+    train_name,
+    source,
+    destination,
+    fare,
+    availability
+  } = req.body;
   console.log("🚀 >>> train_no, train_name, source, destination, fare, availability", train_no, train_name, source, destination, fare, availability);
-  res.status(200).json({ message: "success" });
+  res.status(200).json({
+    message: "success"
+  });
 });
 
-export { router };
+router.delete("/delete-train/:id", async (req, res) => {
+  try {
+    const deletedTrainRecord = await Train.findByIdAndDelete(req.params.id);
+
+    console.log()
+
+    if (!deletedTrainRecord) {
+      console.log("No such train found");
+      res.status(404).send("No such train found. You had entered invalid id");
+    } else {
+      console.log("Train record deleted successfully");
+      res.status(200).send("Train record deleted successfully");
+    }
+  } catch (error) {
+    console.log("Error Occurred while deleting train record");
+    res.status(500).send("Error Occurred while deleting record");
+  }
+})
+
+export {
+  router
+};
